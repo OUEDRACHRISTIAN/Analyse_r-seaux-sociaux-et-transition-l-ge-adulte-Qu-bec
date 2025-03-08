cd0216ad18b1aea129f28bd7a2f1b7ee545f96b8
OUEDRACHRISTIAN <ouedraogocyriaque1@gmail.com>
2025-03-08 21:00:29 +0100

    Ajout de nouveaux dossiers

--- Creation des fichiers.R
+++
@@ -1,6 +0,0 @@
-dir.create("data")       # Dossier pour les données
-dir.create("scripts")    # Dossier pour les scripts R
-dir.create("figures")    # Dossier pour les graphiques
-dir.create("references") # Dossier pour les sources bibliographiques
-
-file.create("main.Rmd")  # Création du fichier R Markdown principal
