ab59df56133de7a658f3bfce7b7cca565c58a5dc
OUEDRACHRISTIAN <ouedraogocyriaque1@gmail.com>
2025-03-08 19:37:31 +0100

    Initial commit

---
+++ Creation des fichiers.R
@@ -0,0 +1,7 @@
+dir.create("data")       # Dossier pour les données
+dir.create("scripts")    # Dossier pour les scripts R
+dir.create("figures")    # Dossier pour les graphiques
+dir.create("references") # Dossier pour les sources bibliographiques
+
+file.create("main.Rmd")  # Création du fichier R Markdown principal
+file.create("README.md") # Ajout d'un fichier README
